6e7210cd40d79ec594c828bbc8848b3f7bba2638
sahil <sahild333@gmail.com>
2021-04-18 18:43:14 -0400

    footerlinks step definitions - Sahil 4/18/21

--- src/test/java/step_definitions/FooterLinkFunctionality.java
+++ src/test/java/step_definitions/FooterLinkFunctionality.java
@@ -1,5 +1,29 @@
 package step_definitions;
 
-public class FooterLinkFunctionality {
+import org.testng.Assert;
 
+import base.TestBase;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.HomePage;
+
+public class FooterLinkFunctionality extends TestBase{
+	
+	HomePage hp;
+
+	@Given("user is in homepage of application")
+	public void user_is_in_homepage_of_application() {
+		driver.get(prop.getProperty("url"));
+	}
+
+	@When("user clicks on About Us footerlink")
+	public void user_clicks_on_about_us_footerlink() {
+	    hp.clickAboutUsFooterLink();
+	}
+
+	@Then("user should land on About Us page")
+	public void user_should_land_on_about_us_page() {
+	    Assert.assertEquals(driver.getTitle(), "About Us - Transfotech Academy");
+	}
 }
